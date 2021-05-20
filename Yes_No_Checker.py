b4a8e896ad20e25b3c78366220eee6bb9bbae1f8
19136PE <>
2021-05-20 22:46:52 +0000

    added instructions code

---
+++ Yes_No_Checker.py
@@ -0,0 +1,31 @@
+#functions Go Here
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("Invalid Input, Please answer yes / no")
+
+def instuctions():
+    print("*** How To Play ***")
+    print()
+    print("Instructions here")
+    print()
+
+#Main Routine Goes Here
+show_instrustions = yes_no("Have You Played The Game Before?")
+print("you chose {}".format(show_instrustions))
+
+if show_instrustions == "no":
+ instuctions()
+
+print("programme continues")
